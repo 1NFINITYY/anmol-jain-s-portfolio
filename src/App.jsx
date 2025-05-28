4fd56106fc31cd58fe14be6c50b961fc0f5d0fd6
1NFINITYY <158573026+1NFINITYY@users.noreply.github.com>
2025-05-28 19:35:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Skills from "./components/skills";
@@ -6,46 +10,31 @@ import Projects from "./components/projects";
 import Contact from "./components/contact";
 import Navbar from "./components/navbar";
 
+// Layout component wraps all pages
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/skills", element: <Skills /> },
+      { path: "/projects", element: <Projects /> },
+    ]
+  }
+]);
 
 const App = () => {
-  
-
-   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Navbar/><Home/>
-    </>
-    },
-
-    {
-      path:"/about",
-      element: <><Navbar/><About/>
-    </>
-    },
-    {
-      path:"/contact",
-      element: <><Navbar/><Contact/>
-    </>
-    },
-    {
-      path:"/skills",
-      element: <><Navbar/><Skills/>
-    </>
-    },
-    {
-      path:"/projects",
-      element: <><Navbar/><Projects/>
-    </>
-    },
-  ])
-
-  return (
-
-    <>
-    <RouterProvider router={router}/>
-    </>
-    
-  )
+  return <RouterProvider router={router} />;
 };
 
 export default App;
+
