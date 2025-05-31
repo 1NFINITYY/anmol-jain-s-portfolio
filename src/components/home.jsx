40c50a3d8a9fdedd1144d9279dbf05ffa2937f34
1NFINITYY <158573026+1NFINITYY@users.noreply.github.com>
2025-05-31 21:17:48 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -29,22 +29,7 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Scroll Indicator */}
-      <div className="mt-16 animate-bounce">
-        <svg
-          className="w-6 h-6 text-pink-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
-      </div>
+      
     </div>
   );
 }
